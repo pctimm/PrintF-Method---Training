0c41d06b8e0f34c7299a44cfcb332f5e88ad95d8
Patrick Timm <86640279+pctimm@users.noreply.github.com>
2021-09-08 18:46:34 -0300

    Paragrafo pronto

--- src/Paragrafo.java
+++ src/Paragrafo.java
@@ -2,8 +2,26 @@
 public class Paragrafo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int linhaCaracteres = 50;
+		int paragrafoCaracteres = 5;
+		
+		String titulo = "As propriedades eletromagnéticas de uma batata"; // seria bom um sublinhado
+		String linha1 = "Batatas são comumente reconhecidas como fonte";
+		String linha2 = "rica de alimentação, para o preparo de diversos";
+		String linha3 = "pratos, de vez em quando gordurosos.";
+		
+		int tituloCaracteres = titulo.length();
+		// System.out.println(tituloCaracteres); 46 caracteres.
+		// int espacosTitulo = (linhaCaracteres - tituloCaracteres) / 2; 2 caracteres
+		
+		char espaco = ' ';
+		System.out.printf("%-2c%s%2c\n", espaco, titulo, espaco);
+		System.out.println();
+		System.out.printf("%-5c%-45s\n", espaco, linha1);
+		System.out.printf("%-50s\n", linha2);
+		System.out.printf("%-50s", linha3);
+		
+		// Não consigo utilizar a variavel linhaCaracteres dentro da String do printf().
 	}
 
 }
