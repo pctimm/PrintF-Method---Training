f9bb2e5dc56c60a40af96ae60f850b60d1522db2
Patrick Timm <86640279+pctimm@users.noreply.github.com>
2021-09-08 18:28:44 -0300

    Abreviação feita
    Olha só, aprendi a cortar strings com o print f :)

--- src/Abreviação.java
+++ src/Abreviação.java
@@ -2,7 +2,18 @@
 public class Abreviação {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String texto = "Oblíquo";  // qualquer palavra legal do dicionário.
+		
+		System.out.printf("%s\n", texto);  // a String inteira.
+		
+		System.out.printf("%.3s.", texto); // . para diminuir o tamanho de caracteres (String).
+		
+		// OBS: Ao usar o número sem o ponto, como tentei: "%3s.", eram mostrados todos os
+		// caracteres da String (então significa: número minimo de caracteres, mas não quer dizer
+		// que serão justamente 3 caracteres).
+		
+		// Agora, com "." antes do número, é feito um corte da String, assim se tem uma
+		// abreviação :)
 
 	}
 
---
+++ src/Paragrafo.java
@@ -0,0 +1,9 @@
+
+public class Paragrafo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
